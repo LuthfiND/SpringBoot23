1538457cef99371c6eacb678f3d8b56f62c01031
Luthfi <oleng1427@gmail.om>
2022-11-01 20:24:13 +0700

    tugas 7

---
+++ src/main/java/id/sinaukoding23/latihan/model/mapper/Category.java
@@ -0,0 +1,20 @@
+package id.sinaukoding23.latihan.model.mapper;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+public class Category extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer categoryId;
+
+    @Column
+    private String categoryName;
+}
